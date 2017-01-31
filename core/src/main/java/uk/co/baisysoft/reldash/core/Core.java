629d7e7a248f2f21d10bb41738a0528a9ae90f8b
Matt Read <mread@spotd.co.uk>
2017-01-31 01:09:51 +0000

    move stuff around

---
+++ core/src/main/java/uk/co/baisysoft/reldash/core/Core.java
@@ -0,0 +1,13 @@
+package uk.co.baisysoft.reldash.core;
+
+import java.util.logging.Logger;
+
+public class Core {
+
+    private static Logger LOG = Logger.getLogger(Core.class.getName());
+
+    public static void main(String[] args) {
+
+
+    }
+}
